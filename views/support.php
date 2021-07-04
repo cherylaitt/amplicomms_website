eae1f68cfa38bc315eea3776ff932c285de5178a
cherylaitt <cherylai481@gmail.com>
2021-07-04 21:52:14 +0800

    Initial commit

---
+++ views/support.php
@@ -0,0 +1,244 @@
+<link rel='stylesheet' type='text/css' href='../css/support.css'>
+<!-- <style>
+body {
+  font-family: sans-serif;
+  margin: 0;
+}
+
+.hero-image {
+    position: relative;
+    text-align: center;
+    color: white;
+}
+
+.hero-text {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+}
+
+.text {
+    margin-top: 50px;
+    margin-left: 100px;
+    width: 900px;
+}
+
+/* textarea rectangle */
+.msg-send{  
+  background: rgb(0, 128, 150);
+}
+.msg-receive{
+  background: rgb(0, 128, 150);
+}
+.msg-send{
+  width: 700px;
+  /* height: 150px; */
+  padding: 5px 5px 5px 10px;
+  margin-top: 80px;
+  margin-left: 130px;
+  border-radius: 3px;
+  line-height: 30px;
+  position: relative;
+  color: white;
+}
+
+.msg-receive{
+  width: 700px;
+  /* height: 150px; */
+  padding: 5px 5px 5px 10px;
+  margin-top: 80px;
+  margin-left: 270px;
+  border-radius: 3px;
+  line-height: 30px;
+  position: relative;
+  color: white;
+}
+
+/* the triangle of receive chat area */
+.msg-receive:before{
+  content: '';
+  width: 0px;
+  height: 0px;
+  position: absolute;
+  border: 15px solid;
+  border-color: transparent rgb(0, 128, 150) transparent transparent;
+  left: -29px;
+  top: 50px;
+}
+.msg-send:after{
+  content: '';
+  width: 0px;
+  height: 0px;
+  position: absolute;
+  border: 15px solid;
+  border-color: transparent transparent transparent rgb(0, 128, 150);
+  right: -29px;
+  top: 50px;
+}
+
+.msg-insert {
+    display: grid;
+    position: static;
+    width: 100%;
+}
+
+.textbox1 {
+    grid-area: 1/1/2/2;
+}
+.textbox2 {
+    grid-area: 3/1/4/2;
+}
+.textbox3 {
+    grid-area: 5/1/6/2;
+}
+.textbox4 {
+    grid-area: 7/1/8/2;
+}
+.textbox5 {
+    grid-area: 9/1/10/2;
+}
+
+.img1 {
+    margin-top: 100px;
+    margin-left: -170px;
+    grid-area: 1/2/2/3;
+}
+.img2 {
+    margin-top: 100px;
+    margin-left: 130px;
+    grid-area: 3/1/4/2;
+}
+.img3 {
+    margin-top: 100px;
+    margin-left: -170px;
+    grid-area: 5/2/6/3;
+}
+.img4 {
+    margin-top: 100px;
+    margin-left: 130px;
+    grid-area: 7/1/8/2;
+}
+.img5 {
+    margin-top: 100px;
+    margin-left: -170px;
+    grid-area: 9/2/10/3;
+}
+
+</style> -->
+
+<body>
+    <div class="hero-image">
+        <img src="../images/support_header.png" alt="" style="width: 100%; height: 400px; object-fit: cover; object-position: 50% 50%;">
+        <div class="hero-text">
+            <h1 style="font-size:50px">Support</h1>
+        </div>
+    </div>
+
+    <div class="text">
+        <p class="font_8" style="font-size:20px">
+            <span style="font-weight:bold">A problem with your Amplicomms telephone ?</span>
+            <br>&nbsp;
+        </p>
+        <ul class="font_8" style="font-size:20px">
+            <li>
+	            <p class="font_8" style="font-size:20px">Download the user guide from the section Downloads/DoC.<br></p>
+	        </li>
+	        <li>
+	            <p class="font_8" style="font-size:20px">Contact the store in which you purchased the telephone for more information<br></p>
+	        </li>
+	        <li>
+	            <p class="font_8" style="font-size:20px">Or contact our Customer Service for any technical question (problem of installation, of use of your product, after-sales service...) at one of the following :</p>
+	        </li>
+        </ul>
+    </div>
+    <div class="msg-insert">
+        <div class="textbox1">
+            <div class="msg-send"> 
+                <p class="font_8" style="font-size:20px">
+                    <span class="color_11">+33 8 26 10 05 80</span>
+                </p>
+                <p class="font_8" style="font-size:20px">
+                    <a href="mailto:France-support@atlinks.com" target="_self">
+                        <span class="color_11">
+                            <span style="text-decoration:underline; color: rgb(255, 255, 255);">france-support@atlinks.com</span>
+                        </span>
+                    </a>
+                </p>
+            </div>
+        </div>
+        <div class="img1">
+            <img src="../images/France.webp" alt="France.png" style="width: 101px; height: 92px; object-fit: cover; object-position: 50% 50%;">
+        </div>
+        <div class="img2">
+            <img src="../images/Spain.webp" alt="Spain.png" style="width: 101px; height: 96px; object-fit: cover; object-position: 50% 50%;">
+        </div>
+        <div class="textbox2">
+            <div class="msg-receive">
+                <p class="font_8" style="text-align:right;font-size:20px">
+                    <span class="color_11">+34 902 636 296</span>
+                </p>
+                <p class="font_8" style="text-align:right;font-size:20px">
+                    <span class="color_11">
+                            <a style="text-decoration:underline; color: rgb(255, 255, 255);" href="mailto:spain-support@atlinks.com" target="_self">spain-support@atlinks.com</a>
+                    </span>
+                </p>
+            </div>
+        </div>
+        <div class="textbox3">
+            <div class="msg-send"> 
+                <p class="font_8" style="font-size:20px">
+                    <span class="color_11">+ 35 1 70 778 00 48</span>
+                </p>
+                <p class="font_8" style="font-size:20px">
+                    <a href="portugal-support@atlinks.com" target="_self">
+                        <span class="color_11">
+                            <span style="text-decoration:underline; color: rgb(255, 255, 255);">portugal-support@atlinks.com</span>
+                        </span>
+                    </a>
+                </p>
+            </div>
+        </div>
+        <div class="img3">
+            <img src="../images/Portugal.webp" alt="Portugal.png" style="width: 101px; height: 92px; object-fit: cover; object-position: 50% 50%;">
+        </div>
+        <div class="img4">
+            <img src="../images/Germany.webp" alt="Germany.png" style="width: 100px; height: 93px; object-fit: cover; object-position: 50% 50%;">
+        </div>
+        <div class="textbox4">
+            <div class="msg-receive">
+                <p class="font_8" style="text-align:right;font-size:20px">
+                <span class="color_11">+49 180 200 1323<br></span>
+                    <span class="color_11">(Preis für ein Ortsgespräch, abhängig von den Kosten, die Ihr Festnetz- oder Mobilfunkbetreiber anbietet)</span>
+                </p>
+                <p class="font_8" style="text-align:right;font-size:20px">
+                    <span class="color_11">
+                            <a style="text-decoration:underline; color: rgb(255, 255, 255);" href="germany-support@atlinks.com" target="_self">spain-support@atlinks.com</a>
+                    </span>
+                </p>
+            </div>
+        </div>
+        <div class="textbox5">
+            <div class="msg-send"> 
+                <p class="font_8" style="font-size:20px">
+                    <span class="color_11">+441355 593597</span>
+                </p>
+                <p class="font_8" style="font-size:20px">
+                    <a href="UK&EI-support@atlinks.com" target="_self">
+                        <span class="color_11">
+                            <span style="text-decoration:underline; color: rgb(255, 255, 255);">UK&EI-support@atlinks.com</span>
+                        </span>
+                    </a>
+                </p>
+            </div>
+        </div>
+        <div class="img5">
+            <img src="../images/United Kingdom.webp" alt="United Kingdom.png" style="width: 101px; height: 96px; object-fit: cover; object-position: 50% 50%;">
+        </div>
+    </div>
+    <br><br><br>
+    
+</body>
+
+<?php include "footer.php" ?>
